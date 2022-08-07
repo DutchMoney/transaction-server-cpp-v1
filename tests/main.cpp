6d87432336eb681dcbc2032bf9e368fce82d7ddb
Preman Jeyabalan <pjab@connect.ust.hk>
2022-08-08 02:47:06 +0800

    updated cmake to ad google test lib

---
+++ tests/main.cpp
@@ -0,0 +1,27 @@
+#include <Transaction.h>
+#include <vector>
+#include <iostream>
+
+int main() {
+    Transaction t {std::vector<Item> {{"apple", 5, 5}, {"banana", 10, 1}}}; 
+
+    std::cout << t << std::endl;
+
+    t.addUserIfNotExists("preman");
+    bool sts  = t.updateUserItem<Transaction::UpdateType::ADD>("preman", {"apple", 2, 5});
+    sts  = t.updateUserItem<Transaction::UpdateType::ADD>("preman", {"banana", 12, 1});
+    sts  = t.updateUserItem<Transaction::UpdateType::ADD>("preman", {"banana", 10, 1});
+    std::cout << t << std::endl;
+    sts  = t.updateUserItem<Transaction::UpdateType::REMOVE>("preman", {"banana", 5, 1});
+
+    std::cout << t << std::endl;
+
+    sts = t.updateItemPrice("banana", 6);
+
+    std::cout << t << std::endl;
+
+    sts = t.removeUser("preman");
+    std::cout << t << std::endl;
+
+    return 0;
+}
